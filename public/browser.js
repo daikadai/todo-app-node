adf3f8651fada3017c4d4503426b2169eaea5a8a
Pham Cao Dai <daikadai1026@gmail.com>
2020-03-28 18:29:07 +0700

    5-Updating a database item(part2)

--- public/browser.js
+++ public/browser.js
@@ -1,11 +1,13 @@
 document.addEventListener('click', function(e) {
   if(e.target.classList.contains('edit-me')) {
-    let userInput = prompt('Enter you desired new text');
-    axios.post('/update-item', {text: userInput})
+    let userInput = prompt('Enter you desired new text', e.target.parentElement.parentElement.querySelector(".item-text").innerHTML);
+    if(userInput) {
+      axios.post('/update-item', {text: userInput, id: e.target.getAttribute('data-id')})
       .then(function() {
-        
+        e.target.parentElement.parentElement.querySelector('.item-text').innerHTML = userInput;
       }).catch(function() {
         console.log("Please try again later");       
-      })
+    })
+    }
   }
 })
