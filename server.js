383baf82eec8365ea2dd88d51844e254ee11771c
Pham Cao Dai <daikadai1026@gmail.com>
2020-03-28 15:23:49 +0700

    3-Connecting Node app to Database

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 let express =  require('express');
+let mongodb = require('mongodb');
 
 let app = express();
+let db ;
+
+let connectionString = 'mongodb+srv://dai123:dai123@todolist-ven11.mongodb.net/TodoApp?retryWrites=true&w=majority';
+mongodb.connect(connectionString, {userNewUrlParser: true}, function(err, client) {
+  db = client.db();
+  app.listen(4000);
+})
 
 app.use(express.urlencoded({extended: false}));
 
@@ -59,8 +67,9 @@ app.get('/', function(req,res) {
 })
 
 app.post('/create-item', function(req, res) {
-  console.log(req.body.item);
-  res.send("Thanks for submitting the form");
+  db.collection('items').insertOne({text: req.body.item }, function() {
+    res.send('Thanks for submitting the form');
+  })
 })
 
-app.listen(4000)
+
